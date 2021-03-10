33b143ced7e46e512697fe177e952b0394b17380
areeb <areeb.vohra@nexuscorp-ltd.com>
2021-03-10 18:35:10 +0500

    feature added

--- lib/screens/DebitCardScreen.dart
+++ lib/screens/DebitCardScreen.dart
@@ -35,16 +35,81 @@ class _DebitCartScreenState extends State<DebitCartScreen> {
                     ),
                   ),
                 ),
-                Material(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xFF7e7e7e)),
+                Container(height: displayHeight(context) * 0.2),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF7e7e7e)),
+                      ),
+                      hintText: 'Debit Card Number (12 digits)'),
+                  maxLength: 12,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly], // Only n
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: displayWidth(context) * 0.44,
+                      child: Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Color(0xFF7e7e7e)),
+                              ),
+                              hintText: 'Expires (MM / YY)'),
+                          maxLength: 5,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
                         ),
-                        hintText: 'Debit Card Number (12 digits)'),
-                    maxLength: 12,
-                    keyboardType: TextInputType.phone,
-                    inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly], // Only n
+                      ),
+                    ),
+                    Container(
+                      width: displayWidth(context) * 0.44,
+                      child: Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Color(0xFF7e7e7e)),
+                              ),
+                              hintText: 'Security code'),
+
+                          maxLength: 3,
+                          keyboardType: TextInputType.number,
+                          inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly], // Only n
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 30, 0, 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Security bythird party',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(height: displayHeight(context) * 0.2),
+                ConstrainedBox(
+                  constraints: BoxConstraints.tightFor(width: displayWidth(context) * 0.90, height: displayHeight(context) * 0.07),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFFeb8310),
+                    ),
+                    onPressed: () {
+                      /*...*/
+                    },
+                    child: Text(
+                      "Save",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
                   ),
                 ),
               ],
